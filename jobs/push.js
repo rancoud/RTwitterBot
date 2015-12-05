dd9e27f27bc606616ae31f6509b11c16985ad365
Sébastien Rancoud <sebastien.rancoud@gmail.com>
2015-12-05 22:31:13 +0100

    clean old job

--- jobs/push.js
+++
@@ -1,8 +0,0 @@
-var client = getTwitterApp(undefined, ['statuses/update']);
-client.post('statuses/update', {status: options[0]},  function(error, tweet, response) {
-  if(error) {
-    logTwitterError(error);
-    throw error;
-  }
-  console.log(tweet);
-});
